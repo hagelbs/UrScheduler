8632a7c017031190b8e567c7775f1e27539cbb8b
lianaparas <37631472+lianaparas@users.noreply.github.com>
2020-01-05 23:42:55 +0700

    Delete constants.dart

--- login and register/lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorPalette {
-  static const primaryColor = Color(0xff70707);
-  static const primaryDarkColor = Color(0xff202020);
-  static const underlineTextField = Color(0xff1c1a1a);
-  static const hintColor = Color(0xff707070);
-}
